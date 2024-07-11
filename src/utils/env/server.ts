d5446021a1c7d33a3c32a1a7d2e18c89b736878a
Giovanni Salas <46335023+ftrbnd@users.noreply.github.com>
2024-07-11 16:19:59 -0700

    Make auth url optional

--- src/utils/env/server.ts
+++ src/utils/env/server.ts
@@ -7,7 +7,7 @@ export const serverEnv = createEnv({
     DIRECT_URL: z.string().url(),
 
     NEXTAUTH_SECRET: z.string(),
-    NEXTAUTH_URL: z.string().url(),
+    NEXTAUTH_URL: z.string().url().optional(),
 
     DISCORD_CLIENT_ID: z.string(),
     DISCORD_CLIENT_SECRET: z.string()
